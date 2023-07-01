f57936c9b73c65f4b5a74994da5c4f9001fed87b
Danial <danial@Danials-MacBook-Pro.local>
2023-07-01 16:42:10 +0330

    FEAT: Add all the needed dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import url from "url";
 
-// https://vitejs.dev/config/
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      components: path.resolve(__dirname, "./src/components"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      layout: path.resolve(__dirname, "./src/layout"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      providers: path.resolve(__dirname, "./src/providers"),
+      styles: path.resolve(__dirname, "./src/styles"),
+      themes: path.resolve(__dirname, "./src/themes"),
+      utils: path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
